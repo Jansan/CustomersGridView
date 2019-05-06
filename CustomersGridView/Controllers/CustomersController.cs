5cc684d9f96b3e4b527c9df391bb8e1f7d31f24a
jansan <jansan-26@hotmail.com>
2019-05-06 12:32:37 +0200

    CustomersController create and action index, UpdateCustomer

---
+++ CustomersGridView/Controllers/CustomersController.cs
@@ -0,0 +1,32 @@
+﻿using CustomersGridView.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CustomersGridView.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            CustomersGridViewEntities entities = new CustomersGridViewEntities();
+            List<Customer> customers = entities.Customers.ToList();
+
+            return View(customers.ToList());
+        }
+        public ActionResult UpdateCustomer(Customer customer)
+        {
+            using (CustomersGridViewEntities entities = new CustomersGridViewEntities())
+            {
+                Customer updatedCustomer = (from c in entities.Customers where c.CustomerId == customer.CustomerId select c).FirstOrDefault();
+                updatedCustomer.Name = customer.Name;
+                updatedCustomer.Country = customer.Country;
+                entities.SaveChanges();
+            }
+            return new EmptyResult();
+        }
+    }
+}
